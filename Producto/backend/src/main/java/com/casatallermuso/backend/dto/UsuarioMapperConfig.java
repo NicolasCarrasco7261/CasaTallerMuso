18ce444438122dd5b28f8bcef754dce014e1fc30
kalimeda <kalimeda@proton.me>
2026-05-14 22:26:17 -0400

    usuario mapper config fix

--- Producto/backend/src/main/java/com/casatallermuso/backend/dto/UsuarioMapperConfig.java
+++ Producto/backend/src/main/java/com/casatallermuso/backend/dto/UsuarioMapperConfig.java
@@ -13,7 +13,7 @@ public interface UsuarioMapperConfig {
     @Mapping(target = "id", ignore = true)
     @Mapping(target = "claveHash", ignore = true)
     @Mapping(target = "activo", ignore = true)
-    @Mapping(target = "tipoUsuario", ignore = true)
+    @Mapping(target = "rol", ignore = true)
     @Mapping(target = "detalleUsuario.id", ignore = true)
     @Named("ignorarCamposCliente")
     void ignorarCamposClienteTemplate(UsuarioDTO.BaseDTO dto, @MappingTarget Usuario entity);
