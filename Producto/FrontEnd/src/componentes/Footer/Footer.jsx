2866fbd89ac6d46c18d8eedf80a60100f014b9c7
kalimeda <kalimeda@proton.me>
2026-06-25 02:47:31 -0400

    agrega footer

---
+++ Producto/FrontEnd/src/componentes/Footer/Footer.jsx
@@ -0,0 +1,34 @@
+import { Link } from "react-router-dom";
+
+export default function Footer() {
+  return (
+    <footer className="navbar navbar-expand-lg pt-4 pb-5 bg-transparent border-top mt-auto">
+      <div className="container" style={{ maxWidth: "1100px" }}>
+        <div className="container d-flex align-items-center justify-content-between">
+          <Link
+            className="navbar-brand fw-bold fs-4 text-dark p-0 m-0"
+            to="/"
+            style={{ letterSpacing: "-1px" }}
+          >
+            <span className="fw-semibold text-muted">Casa taller</span> Mus
+            <span className="text-brand">o</span>
+          </Link>
+
+          <div className="d-flex flex-column gap-2 align-items-end">
+            <div className="d-flex gap-4">
+              <Link to="/cursos" className="text-brand">
+                Cursos
+              </Link>
+              <Link to="/cursos" className="text-brand">
+                Eventos
+              </Link>
+            </div>
+            <p className="fs-6 text-muted fw-semibold">
+              <small>contacto@casatallermuso.cl — Santiago, Chile</small>
+            </p>
+          </div>
+        </div>
+      </div>
+    </footer>
+  );
+}
--- Producto/FrontEnd/src/paginas/Layouts.jsx
+++ Producto/FrontEnd/src/paginas/Layouts.jsx
@@ -2,38 +2,47 @@ import { Outlet, useNavigate } from "react-router-dom";
 import Navbar from "../componentes/Navbar/Navbar";
 import { useAuth } from "../utils/AuthProvider";
 import { useEffect } from "react";
+import Footer from "../componentes/Footer/Footer";
 
 export function PublicLayout() {
-    return <div className="min-vh-100 bg-custom-cream pb-5">
-        <Navbar />
-        <main className="container">
-            <Outlet />
-        </main>
+  return (
+    <div className="d-flex flex-column min-vh-100 bg-custom-cream">
+      <Navbar />
+      <main className="container pb-5">
+        <Outlet />
+      </main>
+      <Footer />
     </div>
+  );
 }
 
 export function AuthLayout() {
-    return <div className="min-vh-100 bg-custom-cream d-flex align-items-center pb-5">
-        <main className="container py-5">
-            <Outlet />
-        </main>
+  return (
+    <div className="min-vh-100 bg-custom-cream d-flex align-items-center pb-5">
+      <main className="container py-5">
+        <Outlet />
+      </main>
     </div>
+  );
 }
 
 export function DashboardLayout() {
-    const { isLoggedIn } = useAuth();
-    const navigate = useNavigate();
+  const { isLoggedIn } = useAuth();
+  const navigate = useNavigate();
 
-    useEffect(() => {
-        if (!isLoggedIn()) {
-            navigate("/auth/login");
-        }
-    }, []);
+  useEffect(() => {
+    if (!isLoggedIn()) {
+      navigate("/auth/login");
+    }
+  }, []);
 
-    return <div className="d-flex flex-column min-vh-100 bg-custom-cream">
-        <Navbar />
-        <main className="container d-flex flex-column flex-grow-1 my-4">
-            <Outlet />
-        </main>
+  return (
+    <div className="d-flex flex-column min-vh-100 bg-custom-cream">
+      <Navbar />
+      <main className="container d-flex flex-column flex-grow-1 my-4">
+        <Outlet />
+      </main>
+      <Footer />
     </div>
+  );
 }
